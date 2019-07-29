3daa97000115a15f95dd2dfcff5fdc2459fdc63d
myname21-meet <myname21@meet.mit.edu>
2019-07-29 15:12:15 -0400

    last part of flask lab

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    fav_food = ['cheeseburger', 'puree batata','apples']
+    return render_template('index.html',
+                           fav_food = fav_food)
 
 if __name__ == '__main__':
    app.run(debug = True)
